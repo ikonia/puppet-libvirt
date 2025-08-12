440cd453b0f30c3e31ac24170327e1c4180071a1
Matt Darcy <matt@projecthugo.co.uk>
2025-08-12 12:55:22 +0100

    set unix_sock_ro_perms to RHEL/Deb default

--- manifests/init.pp
+++ manifests/init.pp
@@ -43,7 +43,7 @@ class libvirt (
   $auth_tcp                  = undef,
   $auth_tls                  = undef,
   $unix_sock_group           = $::libvirt::params::unix_sock_group,
-  $unix_sock_ro_perms        = undef,
+  $unix_sock_ro_perms        = 0777,
   $auth_unix_ro              = $::libvirt::params::auth_unix_ro,
   $unix_sock_rw_perms        = $::libvirt::params::unix_sock_rw_perms,
   $auth_unix_rw              = $::libvirt::params::auth_unix_rw,
